4d4bcce4f096d6e577bb764055eefead71197b5c
arne <arne.driesen@student.kdg.be>
2018-05-15 11:24:44 +0200

    Automatisch ophalen Data

--- MVC/Global.asax.cs
+++ MVC/Global.asax.cs
@@ -7,6 +7,10 @@ using System.Web.Http;
 using System.Web.Optimization;
 using System.Web.Routing;
 using MVC.App_Start;
+using System.Threading;
+using MVC.Controllers.Api;
+using BL;
+using System.Diagnostics;
 
 namespace MVC
 {
@@ -19,6 +23,28 @@ namespace MVC
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            ThreadStart tsTask = new ThreadStart(TaskLoop);
+            Thread MyTask = new Thread(tsTask);
+            MyTask.Start();
+        }
+
+        static void TaskLoop()
+        {
+            while (true)
+            {
+                ScheduledTask();
+                Thread.Sleep(TimeSpan.FromSeconds(10));
+            }
+        }
+
+        static void ScheduledTask()
+        {
+            Debug.WriteLine("Ophalen Data - Start");
+            //TextgainController textgainController = new TextgainController();
+            //DeelplatformenManager deelplatformManager = new DeelplatformenManager();
+            //int id = deelplatformManager.GetDeelplatformByName("Politieke Barometer").DeelplatformId;
+            //textgainController.HaalBerichtenOp(deelplatformManager.GetDeelplatform(id));
+            //Debug.WriteLine("Ophalen Data - Done");
         }
     }
 }
