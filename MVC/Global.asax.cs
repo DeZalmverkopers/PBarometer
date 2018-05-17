3e2278f870e1b1d4e493bd307a4612a79aa8eb4f
arne <arne.driesen@student.kdg.be>
2018-05-17 19:50:22 +0200

    commit before deployment

--- MVC/Global.asax.cs
+++ MVC/Global.asax.cs
@@ -25,9 +25,9 @@ namespace MVC
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-            ThreadStart tsTask = new ThreadStart(TaskLoop);
-            Thread MyTask = new Thread(tsTask);
-            MyTask.Start();
+            //ThreadStart tsTask = new ThreadStart(TaskLoop);
+            //Thread MyTask = new Thread(tsTask);
+            //MyTask.Start();
         }
 
         [Conditional("DEBUG")]
@@ -36,45 +36,45 @@ namespace MVC
             TelemetryConfiguration.Active.DisableTelemetry = true;
         }
 
-        static void TaskLoop()
-        {
-            while (true)
-            {
-                ScheduledTask1();
-                Thread.Sleep(TimeSpan.FromSeconds(10));
-                ScheduledTask2();
-                Thread.Sleep(TimeSpan.FromMinutes(1));
-            }
-        }
+        //static void TaskLoop()
+        //{
+        //    while (true)
+        //    {
+        //        ScheduledTask1();
+        //        Thread.Sleep(TimeSpan.FromSeconds(10));
+        //        ScheduledTask2();
+        //        Thread.Sleep(TimeSpan.FromMinutes(1));
+        //    }
+        //}
 
-        static void ScheduledTask1()
-        {
-            Debug.WriteLine("Ophalen Data 1 - Start");
-            DeelplatformenManager deelplatformenManager = new DeelplatformenManager();
-            int id = deelplatformenManager.GetDeelplatformByName("Politieke Barometer").DeelplatformId;
-            GemonitordeItemsManager gemonitordeItemsManager = new GemonitordeItemsManager();
-            gemonitordeItemsManager.AddOrganisatie("Open VLD", id, new List<string>() { "Alexander De Croo", "Gwendolyn Rutten", "Maggie De Block" });
-            gemonitordeItemsManager.AddOrganisatie("Groen", id, new List<string>() { "Kristof Calvo", "Meyrem Almaci", "Wouter Van Besien" });
-            gemonitordeItemsManager.AddOrganisatie("SPA", id, new List<string>() { "Caroline Gennez", "John Crombez", "Bruno Tobback" });
-            gemonitordeItemsManager.AddOrganisatie("Vlaams Belang", id, new List<string>() { "Filip Dewinter", "Tom Van Grieken", "Gerolf Annemans" });
+        //static void ScheduledTask1()
+        //{
+        //    Debug.WriteLine("Ophalen Data 1 - Start");
+        //    DeelplatformenManager deelplatformenManager = new DeelplatformenManager();
+        //    int id = deelplatformenManager.GetDeelplatformByName("Politieke Barometer").DeelplatformId;
+        //    GemonitordeItemsManager gemonitordeItemsManager = new GemonitordeItemsManager();
+        //    gemonitordeItemsManager.AddOrganisatie("Open VLD", id, new List<string>() { "Alexander De Croo", "Gwendolyn Rutten", "Maggie De Block" });
+        //    gemonitordeItemsManager.AddOrganisatie("Groen", id, new List<string>() { "Kristof Calvo", "Meyrem Almaci", "Wouter Van Besien" });
+        //    gemonitordeItemsManager.AddOrganisatie("SPA", id, new List<string>() { "Caroline Gennez", "John Crombez", "Bruno Tobback" });
+        //    gemonitordeItemsManager.AddOrganisatie("Vlaams Belang", id, new List<string>() { "Filip Dewinter", "Tom Van Grieken", "Gerolf Annemans" });
 
-            gemonitordeItemsManager.AddThema("Migratie", new List<string>() { "buitenland", "vluchteling", "immigratie", "migratie" }, id);
-            gemonitordeItemsManager.AddThema("Fiscaliteit", new List<string>() { "belastingen", "tax", "btw", "sociale zekerheid" }, id);
-            gemonitordeItemsManager.AddThema("Milieu", new List<string>() { "kernenergie", "zonnenergie", "steenkool", "luchtvervuiling", "windenergie" }, id);
+        //    gemonitordeItemsManager.AddThema("Migratie", new List<string>() { "buitenland", "vluchteling", "immigratie", "migratie" }, id);
+        //    gemonitordeItemsManager.AddThema("Fiscaliteit", new List<string>() { "belastingen", "tax", "btw", "sociale zekerheid" }, id);
+        //    gemonitordeItemsManager.AddThema("Milieu", new List<string>() { "kernenergie", "zonnenergie", "steenkool", "luchtvervuiling", "windenergie" }, id);
 
             
-            Debug.WriteLine("Ophalen Data 2 - Done");
-        }
+        //    Debug.WriteLine("Ophalen Data 2 - Done");
+        //}
 
-        static void ScheduledTask2()
-        {
-            Debug.WriteLine("Invoegen Textgain 1 - Start");
-            TextgainController textgainController = new TextgainController();
-            DeelplatformenManager deelplatformManager = new DeelplatformenManager();
+        //static void ScheduledTask2()
+        //{
+        //    Debug.WriteLine("Invoegen Textgain 1 - Start");
+        //    TextgainController textgainController = new TextgainController();
+        //    DeelplatformenManager deelplatformManager = new DeelplatformenManager();
 
-            int id = deelplatformManager.GetDeelplatformByName("Politieke Barometer").DeelplatformId;
-            textgainController.HaalBerichtenOp(deelplatformManager.GetDeelplatform(id));
-            Debug.WriteLine("Invoegen Textgain 2 - Done");
-        }
+        //    int id = deelplatformManager.GetDeelplatformByName("Politieke Barometer").DeelplatformId;
+        //    textgainController.HaalBerichtenOp(deelplatformManager.GetDeelplatform(id));
+        //    Debug.WriteLine("Invoegen Textgain 2 - Done");
+        //}
     }
 }
