418b9e1b7ad99532fb3d735efca94286a9d2fe1b
arne <arne.driesen@student.kdg.be>
2018-05-18 10:20:15 +0200

    ophaalfrequentie toegevoegd + global.asax'

--- Domain/Deelplatformen/Deelplatform.cs
+++ Domain/Deelplatformen/Deelplatform.cs
@@ -5,23 +5,25 @@ using System.Collections.Generic;
 
 namespace Domain.Deelplatformen
 {
-  public class Deelplatform
-  {
-    public int DeelplatformId { get; set; }
-    public string Naam { get; set; }
-    public int AantalDagenHistoriek { get; set; }
-    public DateTime LaatsteSynchronisatie { get; set; }
-    public string AfbeeldingPad { get; set; }
-    public string Achtergrondkleur { get; set; }
-    public string URLnaam { get; set; }
-    public List<GemonitordItem> GemonitordeItems { get; set; }
-
-    public Deelplatform()
+    public class Deelplatform
     {
-      GemonitordeItems = new List<GemonitordItem>();
+        public int DeelplatformId { get; set; }
+        public string Naam { get; set; }
+        public int AantalDagenHistoriek { get; set; }
+        public DateTime LaatsteSynchronisatie { get; set; }
+        public string AfbeeldingPad { get; set; }
+        public string Achtergrondkleur { get; set; }
+        public string URLnaam { get; set; }
+        public int DataOphaalFrequentie { get; set; }
+
+        public List<GemonitordItem> GemonitordeItems { get; set; }
+
+        public Deelplatform()
+        {
+            GemonitordeItems = new List<GemonitordItem>();
+        }
+        public bool OverzichtAdded { get; set; }
+        public bool WeeklyReviewAdded { get; set; }
+        public bool AlertsAdded { get; set; }
     }
-    public bool OverzichtAdded { get; set; }
-    public bool WeeklyReviewAdded { get; set; }
-    public bool AlertsAdded { get; set; }
-  }
 }
--- MVC/Global.asax.cs
+++ MVC/Global.asax.cs
@@ -12,6 +12,7 @@ using MVC.Controllers.Api;
 using BL;
 using System.Diagnostics;
 using Microsoft.ApplicationInsights.Extensibility;
+using Domain.Deelplatformen;
 
 namespace MVC
 {
@@ -25,9 +26,9 @@ namespace MVC
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-            //ThreadStart tsTask = new ThreadStart(TaskLoop);
-            //Thread MyTask = new Thread(tsTask);
-            //MyTask.Start();
+            ThreadStart tsTask = new ThreadStart(TaskLoop);
+            Thread MyTask = new Thread(tsTask);
+            MyTask.Start();
         }
 
         [Conditional("DEBUG")]
@@ -36,45 +37,52 @@ namespace MVC
             TelemetryConfiguration.Active.DisableTelemetry = true;
         }
 
-        //static void TaskLoop()
-        //{
-        //    while (true)
-        //    {
-        //        ScheduledTask1();
-        //        Thread.Sleep(TimeSpan.FromSeconds(10));
-        //        ScheduledTask2();
-        //        Thread.Sleep(TimeSpan.FromMinutes(1));
-        //    }
-        //}
+        static void TaskLoop()
+        {
+            while (true)
+            {
+                ScheduledTask1();
+                Thread.Sleep(TimeSpan.FromSeconds(10));
+                ScheduledTask2();
+                Thread.Sleep(TimeSpan.FromMinutes(1));
+            }
+        }
 
-        //static void ScheduledTask1()
-        //{
-        //    Debug.WriteLine("Ophalen Data 1 - Start");
-        //    DeelplatformenManager deelplatformenManager = new DeelplatformenManager();
-        //    int id = deelplatformenManager.GetDeelplatformByName("Politieke Barometer").DeelplatformId;
-        //    GemonitordeItemsManager gemonitordeItemsManager = new GemonitordeItemsManager();
-        //    gemonitordeItemsManager.AddOrganisatie("Open VLD", id, new List<string>() { "Alexander De Croo", "Gwendolyn Rutten", "Maggie De Block" });
-        //    gemonitordeItemsManager.AddOrganisatie("Groen", id, new List<string>() { "Kristof Calvo", "Meyrem Almaci", "Wouter Van Besien" });
-        //    gemonitordeItemsManager.AddOrganisatie("SPA", id, new List<string>() { "Caroline Gennez", "John Crombez", "Bruno Tobback" });
-        //    gemonitordeItemsManager.AddOrganisatie("Vlaams Belang", id, new List<string>() { "Filip Dewinter", "Tom Van Grieken", "Gerolf Annemans" });
+        static void ScheduledTask1()
+        {
+            Debug.WriteLine("Ophalen Data 1 - Start");
+            DeelplatformenManager deelplatformenManager = new DeelplatformenManager();
+            int id = deelplatformenManager.GetDeelplatformByName("Politieke Barometer").DeelplatformId;
+            GemonitordeItemsManager gemonitordeItemsManager = new GemonitordeItemsManager();
+            gemonitordeItemsManager.AddOrganisatie("Open VLD", id, new List<string>() { "Alexander De Croo", "Gwendolyn Rutten", "Maggie De Block" });
+            gemonitordeItemsManager.AddOrganisatie("Groen", id, new List<string>() { "Kristof Calvo", "Meyrem Almaci", "Wouter Van Besien" });
+            gemonitordeItemsManager.AddOrganisatie("SPA", id, new List<string>() { "Caroline Gennez", "John Crombez", "Bruno Tobback" });
+            gemonitordeItemsManager.AddOrganisatie("Vlaams Belang", id, new List<string>() { "Filip Dewinter", "Tom Van Grieken", "Gerolf Annemans" });
 
-        //    gemonitordeItemsManager.AddThema("Migratie", new List<string>() { "buitenland", "vluchteling", "immigratie", "migratie" }, id);
-        //    gemonitordeItemsManager.AddThema("Fiscaliteit", new List<string>() { "belastingen", "tax", "btw", "sociale zekerheid" }, id);
-        //    gemonitordeItemsManager.AddThema("Milieu", new List<string>() { "kernenergie", "zonnenergie", "steenkool", "luchtvervuiling", "windenergie" }, id);
+            gemonitordeItemsManager.AddThema("Migratie", new List<string>() { "buitenland", "vluchteling", "immigratie", "migratie" }, id);
+            gemonitordeItemsManager.AddThema("Fiscaliteit", new List<string>() { "belastingen", "tax", "btw", "sociale zekerheid" }, id);
+            gemonitordeItemsManager.AddThema("Milieu", new List<string>() { "kernenergie", "zonnenergie", "steenkool", "luchtvervuiling", "windenergie" }, id);
 
-            
-        //    Debug.WriteLine("Ophalen Data 2 - Done");
-        //}
 
-        //static void ScheduledTask2()
-        //{
-        //    Debug.WriteLine("Invoegen Textgain 1 - Start");
-        //    TextgainController textgainController = new TextgainController();
-        //    DeelplatformenManager deelplatformManager = new DeelplatformenManager();
+            Debug.WriteLine("Ophalen Data 2 - Done");
+        }
 
-        //    int id = deelplatformManager.GetDeelplatformByName("Politieke Barometer").DeelplatformId;
-        //    textgainController.HaalBerichtenOp(deelplatformManager.GetDeelplatform(id));
-        //    Debug.WriteLine("Invoegen Textgain 2 - Done");
-        //}
+        static void ScheduledTask2()
+        {
+            Debug.WriteLine("Invoegen Textgain 1 - Start");
+            TextgainController textgainController = new TextgainController();
+            DeelplatformenManager deelplatformManager = new DeelplatformenManager();
+            IEnumerable<Deelplatform> deelplatformen = deelplatformManager.GetDeelplatformen();
+
+            foreach (var deelplatform in deelplatformen) 
+            {
+                // Volgens mij gaan we dit niet met een thread kunnnen doen want je wil de delays (bij while true) weg hebben. 
+                // mss eerder iets van een timer gebruiken die gewoon om de x aantal minuten (hangt af van deelplatform ) start ? 
+
+            }
+            int id = deelplatformManager.GetDeelplatformByName("Politieke Barometer").DeelplatformId;
+            textgainController.HaalBerichtenOp(deelplatformManager.GetDeelplatform(id));
+            Debug.WriteLine("Invoegen Textgain 2 - Done");
+        }
     }
 }
